8f84fbd5c4903b3d8ba04d7e42a33b4da87e86c9
schwester34 <yana64604@gmail.com>
2022-02-27 18:32:03 +0500

    Initial commit

---
+++ src/test/java/github/DragAndDrope.java
@@ -0,0 +1,33 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDrope {
+        @BeforeAll
+        static void beforeAll() {
+            Configuration.browserSize = "1920x1080";
+            Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        }
+
+        @AfterAll
+        static void AfterAll() {
+            Configuration.holdBrowserOpen = true;
+        }
+
+        @Test
+        void dragAndDropTo() {
+
+            open("/drag_and_drop");
+            $("#column-a").dragAndDropTo("#column-b");
+            $$("#column-a").shouldHave(texts("B"));
+            $$("#column-b").shouldHave(texts("A"));
+
+        }
+    }
+
---
+++ src/test/java/github/Testt.java
@@ -0,0 +1,33 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class Testt {
+   @BeforeAll
+    static void beforeAll () {
+       Configuration.baseUrl = "https://github.com";
+       Configuration.browserSize = "1920x1080";
+   }
+
+    @Test
+    void  PagesContentSoftAssertionsAndJUnit5(){
+       // открыть страницу github.com
+        open("https://github.com/");
+        // Перейти в раздел Wiki проекта
+        $("#wiki-tab").click();
+        // В списке страниц (Pages) ищем SoftAssertions
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+        // Кликаем по SoftAssertions
+        $$(".filterable-active a").findBy(text("SoftAssertions")).click();
+        // Ищем кусок кода для JUnit5
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})"));
+    }
+
+
+}
