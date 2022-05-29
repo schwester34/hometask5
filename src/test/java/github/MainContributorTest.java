27fae4fe89e31d42cf422f8105d29672b5d7f2dc
schwester34 <99205353+schwester34@users.noreply.github.com>
2022-05-29 17:46:46 +0500

    Update MainContributorTest.java

--- src/test/java/github/MainContributorTest.java
+++ src/test/java/github/MainContributorTest.java
@@ -10,13 +10,13 @@ import static com.codeborne.selenide.Selenide.*;
 public class MainContributorTest {
     @Test
     void andreiSolntsevShouldBeTheMainContributor() {
-        // открыть страничку репозитория Селенида
+        // открыть страничку репозитория selenide
         open("https://github.com/selenide/selenide");
         // подвести мышку к первому элементу в области Contributors
         $(".Layout-sidebar").$(withText("Contributors"))
                 .closest(".BorderGrid-row").$("ul li").hover();
 
-        // check: в появившемся окошке (оверлей) текст Andrei Solntsev
+        // в появившемся окошке текст Andrei Solntsev
         $$(".Popover-message").findBy(visible).shouldHave(text("Andrei Solntsev"));
 
     }
